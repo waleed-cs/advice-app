64f602af5d71207b933a10e4baa2931794c55d19
waleed-cs <waleedalharthi1999@gmail.com>
2022-08-21 08:07:28 +0300

    first commit

---
+++ src/components/Advice.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "../styles/Advice.scss";
+import dividerImage from "../images/pattern-divider-desktop.svg";
+import diceImage from "../images/icon-dice.svg";
+const Advice = () => {
+  const [adviceData, setAdviceData] = useState(null);
+  const fetchAdvice = async () => {
+    const { data } = await axios.get(
+      `https://api.adviceslip.com/advice?t=${Math.random()}`
+    );
+    console.log(data.slip);
+    setAdviceData(data.slip);
+  };
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+  return (
+    <>
+      {adviceData && (
+        <div className="container">
+          <div className="advice_output">
+            <h1 className="title">Advice #{adviceData.id}</h1>
+            <p className="advice">
+              <q>{adviceData.advice}</q>
+            </p>
+          </div>
+          <img className="divider" src={dividerImage} alt="divider" />
+          <button className="dice" onClick={() => fetchAdvice()}>
+            <img src={diceImage} alt="dice" />
+          </button>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Advice;
---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Advice from "./Advice";
+const App = () => {
+  return (
+    <>
+      <Advice />
+    </>
+  );
+};
+
+export default App;
